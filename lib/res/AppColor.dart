119295d6162b82516f8c0e698992f14bc81d57c2
Yazan Moqanasa <150680289+YazanMoqanasaa@users.noreply.github.com>
2024-05-06 22:21:07 +0300

    Update Color

--- lib/res/AppColor.dart
+++ lib/res/AppColor.dart
@@ -6,6 +6,6 @@ class AppColor{
   static const Color validation = Color(0xFFFA0F0F);
   static const Color productInfo = Color(0xFF3E7DA9);
   static const Color grey = Color(0xFF868686);
-  static const Color greylight = Color(0xD9D9D9);
+  static const Color greylight = Color(0xD4D9D9D9);
 
 }
