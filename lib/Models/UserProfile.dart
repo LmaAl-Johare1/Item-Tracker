85bc2e09abd2761ea76aa4dc5738df6edca09909
yazan moqanasa <ymonqanasa@kabi.ai>
2024-05-27 13:43:20 +0300

    Added profile manager UI and logic and update settingView

---
+++ lib/Models/UserProfile.dart
@@ -0,0 +1,13 @@
+class UserProfile {
+  String businessName;
+  String businessAddress;
+  String phoneNumber;
+  String email;
+
+  UserProfile({
+    this.businessName = '',
+    this.businessAddress = '',
+    this.phoneNumber = '',
+    this.email = '',
+  });
+}
---
+++ lib/Views/setting/settingview.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // Import generated localizations
+import 'package:project/res/AppColor.dart';
+import 'package:project/res/AppText.dart';
+
+import '../../ViewModels/settingviewmodel.dart';
+import '../dashboard/DashboardView.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  int _selectedIndex = 2;
+  final settingviewmodel _authService = settingviewmodel();
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (_selectedIndex) {
+      case 0:
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ReportsPage()));
+        break;
+      case 1:
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MyHomePage()));
+        break;
+      case 2:
+      // Already on Settings Page
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!; // Retrieve localized strings
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          localizations.settings,
+          style: TextStyle(
+            fontSize: AppText.headingOne.fontSize,
+            fontWeight: AppText.headingOne.fontWeight,
+            color: AppColor.primary,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 0),
+          Container(
+            margin: EdgeInsets.all(22),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 0,
+                  blurRadius: 10,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: ListTile(
+              title: Text(
+                localizations.profile,
+                style: TextStyle(
+                  fontSize: AppText.headingTwo.fontSize,
+                  fontWeight: AppText.headingTwo.fontWeight,
+                  color: AppColor.primary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              trailing: Icon(
+                Icons.navigate_next,
+                color: AppColor.primary,
+                size: 35,
+              ),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/Profile');
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(22),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 0,
+                  blurRadius: 10,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: ListTile(
+              title: Text(
+                localizations.addAccount,
+                style: TextStyle(
+                  fontSize: AppText.headingTwo.fontSize,
+                  fontWeight: AppText.headingTwo.fontWeight,
+                  color: AppColor.primary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              trailing: Icon(
+                Icons.navigate_next,
+                color: AppColor.primary,
+                size: 35,
+              ),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/signup');
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(22),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  spreadRadius: 0,
+                  blurRadius: 10,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: ExpansionTile(
+              title: Text(
+                localizations.language,
+                style: TextStyle(
+                  fontSize: AppText.headingTwo.fontSize,
+                  fontWeight: AppText.headingTwo.fontWeight,
+                  color: AppColor.primary,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              trailing: Icon(
+                Icons.navigate_next,
+                color: AppColor.primary,
+                size: 35,
+              ),
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                    localizations.english,
+                    style: TextStyle(
+                      fontSize: AppText.headingTwo.fontSize,
+                      fontWeight: AppText.headingTwo.fontWeight,
+                      color: AppColor.primary,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  onTap: () {
+                    // Handle language change to English
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    localizations.arabic,
+                    style: TextStyle(
+                      fontSize: AppText.headingTwo.fontSize,
+                      fontWeight: AppText.headingTwo.fontWeight,
+                      color: AppColor.primary,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  onTap: () {
+                    // Handle language change to Arabic
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 150),
+          ListTile(
+            title: Text(
+              localizations.logout, // Use the logout localization key
+              style: TextStyle(
+                fontSize: AppText.headingTwo.fontSize,
+                fontWeight: AppText.headingTwo.fontWeight,
+                color: AppColor.validation,
+              ),
+            ),
+            leading: Icon(
+              Icons.logout,
+              color: AppColor.productInfo,
+              size: 40,
+            ),
+            onTap: () {
+              _authService.signOut(context);
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart),
+            label: localizations.reports,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: localizations.home,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: localizations.settings,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
