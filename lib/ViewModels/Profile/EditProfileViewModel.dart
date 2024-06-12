c29c4f1e5cc7f307f296d2267c2b1790b7047587
zahaaMuhanna <106498282+zahaaMuhanna@users.noreply.github.com>
2024-06-12 20:32:55 +0300

    Update EditProfileViewModel.dart

--- lib/ViewModels/Profile/EditProfileViewModel.dart
+++ lib/ViewModels/Profile/EditProfileViewModel.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../../Models/UserModel.dart';
 
 /// ViewModel for managing and editing the user profile data.
